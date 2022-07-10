09e3b8ea862bb8550e39ec9fa5b21cba4c755a7a
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-07-11 00:08:26 +0500

    Get list of accounts using ganache

--- blockchain/test/inbox.test.js
+++ blockchain/test/inbox.test.js
@@ -3,3 +3,17 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+    //Get a List of all accounts
+    web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+        console.log(fetchedAccounts);
+    })
+    .catch(error => console.log(error));
+
+});
+
+it('describes accounts', () => {
+
+});

620531b0df7927508fc67950e9fbd37e148a87ef
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-07-11 00:36:15 +0500

    Deploy contract

--- blockchain/test/inbox.test.js
+++ blockchain/test/inbox.test.js
@@ -1,16 +1,24 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const {AbiInterface, ByteCodeObject } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
 let accounts;
+let inbox;
+
 beforeEach(async () => {
     //Get a List of all accounts async
     accounts = await web3.eth.getAccounts();
 
+    //Use one of those account to deploy the contract
+    inbox = await new web3.eth.Contract(AbiInterface)
+            .deploy({data: ByteCodeObject, arguments: ['Hi There!']})
+            .send({from: accounts[0], gas: '1000000'});
 });
 
 it('describes accounts', () => {
-    console.log(accounts);
+    console.log('Etherium Accounts = ', accounts);
+    console.log('Inbox Account = ', inbox);
 });

9369353d5cdbd3431335c6aee369a701256fab7a
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-07-17 08:17:39 +0500

    Validate that ether/wei amount cannot be less than specified in contract

--- blockchain/test/Lottery.test.js
+++ blockchain/test/Lottery.test.js
@@ -9,6 +9,7 @@ const {AbiInterface, ByteCodeObject} = require('../compile');
 let lottery;
 let accounts;
 
+//Deploy Contract and getAccounts 
 beforeEach( async () => {
     accounts = await web3.eth.getAccounts();
     lottery = await new web3.eth.Contract(AbiInterface)
@@ -16,12 +17,15 @@ beforeEach( async () => {
     .send({from: accounts[0], gas: '1000000'})
 });
 
+
 describe('Lottery Contract', () => {
+
+    //Validate if the contract is deployed successfully!
     it('Deploys a Contract', () => {
-        //Validate if contract is deployed successfully!
         assert.ok(lottery.options.address);
     });
 
+    //Add One User in the Lottery and validate
     it('allows one account to enter', async () => {
         await lottery.methods.enter().send({
             from: accounts[0],
@@ -36,6 +40,7 @@ describe('Lottery Contract', () => {
         assert.equal(accounts[0], players[0]);
     });
 
+    //Enter multiple users in the lottery and validate
     it('allows multiple accounts to enter', async () => {
         await lottery.methods.enter().send({
             from: accounts[0],
@@ -61,5 +66,19 @@ describe('Lottery Contract', () => {
         assert.equal(accounts[1], players[1]);
         assert.equal(accounts[2], players[2]);
 
-    })
+    });
+
+    //Validate that ether/wei amount cannot be less than specified in contract
+    it('requires a minimum amount of ether to enter', async() => {
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 10
+            });
+            assert(false);      
+        } catch (error) {
+            assert(error);
+        }
+
+    });
 })
